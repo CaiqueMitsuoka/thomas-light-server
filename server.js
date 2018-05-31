77f0081068fc60ce6ed56781952e106ad55ef251
Caique Hitoshi Mitsuoka <caique.mitsuoka@gmail.com>
2018-05-31 17:20:46 -0300

    Add the renamed eureka to thomas light server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const PORT = process.env.PORT || 8080
+const router = require('./routes/router')(server)
+
+app.set('view engine', 'ejs')
+app.use('/', router)
+
+server.listen(PORT, () => {
+  console.log(`[THOMAS-LIGHT-SERVER] Listening on: ${PORT}`)
+})
